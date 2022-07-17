e31bdfe1574b2f18679efeaa8d744718e8e2a951
167848.SIGIT <167848.SIGIT@AHI-PYR-167848>
2022-07-17 16:38:47 +0700

    Add project files.

---
+++ Skripsi/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Skripsi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Skripsi.Controllers
+{
+    public class HomeController : Controller
+    {
+       public ActionResult Index()
+        {
+            //if (Session["sessionUser"] == null)
+            //{
+            //    return RedirectToAction("signout");
+            //}
+
+            //Users user = (Users)Session["sessionUser"];
+            return View();
+        }
+
+        public ActionResult SignOut()
+        {
+
+            Session["sessionUser"] = null;
+            return View("index", "login");
+
+        }
+    }
+}
